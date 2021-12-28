5009df6748f67220729c3ad7f9d626ed05fb8fa7
avilldaniel <90011911+avilldaniel@users.noreply.github.com>
2021-12-28 15:08:13 -0800

    failed attempt at overriding Bootstrap themes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import './custom.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import './custom.scss';
 
 ReactDOM.render(
   <React.StrictMode>
